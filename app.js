297ec66aeb37dcf2d868458d1347d7c7256d953e
Vipul Dang <vipuldang1998@gmail.com>
2020-08-05 22:55:54 +0530

    f

---
+++ app.js
@@ -0,0 +1,37 @@
+const request = require("request");
+const express= require('express');
+const app=express();
+// API endpoint to which the http
+// request will be made
+const bodyParser=require('body-parser');
+const exphbs=require('express-handlebars');
+const path=require('path');
+const abc =require('novelcovid');
+
+
+
+app.use(express.static(path.join(__dirname,'public')));
+app.engine('handlebars',exphbs({defaultLayout: 'main'}));
+app.set('view engine','handlebars');
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+  //abc.states({state : 'india'}).then(resu=>{
+    //console.log(resu)
+  //})
+  abc.countries({sort : 'cases'}).then((result)=>{
+    //console.log(result);
+    res.render('main',{result : result});
+  })
+
+});
+
+
+var port = process.env.PORT | 9000;
+app.listen(port,()=>{
+  console.log(`listening to port `+port);
+});
